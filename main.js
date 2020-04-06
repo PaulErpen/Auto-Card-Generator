ab483a485779478c495d8e55de8221039cc8457f
Paul Erpenstein <paulerpenstein@yahoo.de>
2020-04-06 17:39:11 +0200

    basics working

--- main.js
+++ main.js
@@ -1,9 +1,14 @@
-var Mustache = require('mustache');
-var fs = require('fs')
+const Mustache = require('mustache');
+const fs = require('fs');
+const bodyParser = require('body-parser');
+const multiparty = require('multiparty');
 
 const express = require('express')
 const app = express()
 const port = 3000
+
+//use a body parser
+app.use(bodyParser.json());
 
 //make modules and such accessible
 app.use(express.static('public'))
@@ -11,7 +16,61 @@ app.use(express.static('public'))
 app.get('/', (req, res) => res.send(Mustache.render(fs.readFileSync("templates/main.mustache").toString('utf8'), {})))
 
 app.post('/excel', function(req, res) {
-    var workbook = xlsx.read(binary, {type : "binary"});
+    //var workbook = xlsx.read(binary, {type : "binary"});
+    var form = new multiparty.Form();
+    form.parse(req, function(err, fields, files) {
+        if(!err) {
+            var XLSX = require('js-xlsx');
+            var workbook = XLSX.readFile(files.file[0].path);
+            var template_data = convertToTemplateData(workbook);
+            res.send(Mustache.render(fs.readFileSync("templates/cards.mustache").toString('utf8'), {"cards" : template_data}));
+        }
+    });
 });
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+
+function convertToTemplateData(workbook) {
+    var row_indieces = ["A", "B", "C", "D"];
+
+    var cards = []
+    for(k in workbook["SheetNames"]) {
+        var current_sheet = workbook["Sheets"][workbook["SheetNames"][k]];
+        var count_cards = get_word_count(current_sheet, row_indieces)
+
+        for(i = 2; i <= count_cards; i++) {
+            var card = {};
+            for(j = 0; j < row_indieces.length; j++) {
+                var value = current_sheet["" + row_indieces[j] + i]
+                switch (j) {
+                    case 0:
+                        card["title"] = value.w;
+                        break;
+                    case 1:
+                        card["description"] = value.w;
+                        break;
+                    case 2:
+                        card["effect"] = value.w;
+                        break;
+                    case 3:
+                        card["quantity"] = value.v;
+                        break;
+                }
+            }
+            for(l = 0; l < card["quantity"]; l++){
+                cards.push(card);
+            }
+        }
+    }
+    return cards;
+}
+
+function get_word_count(current_sheet, row_indieces) {
+    var i = 0;
+    for(k in current_sheet) {
+        if(k[0] != "!") {
+            i++;
+        }
+    }
+    return i / row_indieces.length;
+}
--- public/js/client.js
+++ public/js/client.js
@@ -1,24 +1,59 @@
 require.config({
   paths: {
       'html2canvas': 'html2canvas.min',
-      'jquery': 'jquery.min',
-      'xslx': 'xlsx'
+      'jquery': 'jquery.min'
   }
 });
 
-require(['html2canvas', 'jquery', 'xlsx'], function(html2canvas, $) {
+require(['html2canvas', 'jquery'], function(html2canvas, $) {
   $("#process").on("click", processData);
-  debugger;
+  $("#print").on("click", printCards);
 
   function processData(event) {
     var files = $("#data").get(0).files;
     if(files.length > 0) {
-      var fr = new FileReader();
-      debugger;
-      fr.onload = function(f) {
-        var binary =  f.target.result;
+      sendExcelData(files[0])
+    }
+  }
+
+  function sendExcelData(file) {
+    var fd = new FormData();
+    fd.append('file', file);
+    $.ajax({
+      url: "http://localhost:3000/excel",
+      type: 'post',
+      data: fd,
+      contentType: false,
+      processData: false,
+      success: handleResponse
+    });
+  }
+
+  function handleResponse(data) {
+    $("#preview").html(data);
+    var effect_fields = $(".card .effect");
+    var max_height = 0;
+    for(k in effect_fields) {
+      if(effect_fields[k].offsetHeight > max_height) {
+        max_height = effect_fields[k].offsetHeight
       }
-      fr.readAsBinaryString(files[0]);
+    }
+    $(".card .effect").css("bottom", max_height / 2 + "px");
+  }
+
+  function printCards(event) {
+    var cards = $(".card");
+    for(k in cards) {
+      html2canvas(cards[k]).then(function(canvas) {
+        var url = canvas.toDataURL("image/png");
+        var link = document.createElement('a');
+        link.href = url;
+        link.download = 'Download.png';
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+      });
+      return;
     }
   }
 });
